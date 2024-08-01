490d460b1a2f3f62299cc028e7eaa65b659e7f5d
rikikangsc2-eng <nueapi11@gmail.com>
2024-08-01 03:54:22 +0000

    Anjay

--- RickyPurPur.js
+++ RickyPurPur.js
@@ -20,7 +20,7 @@ const botOwner = '6283894391287';
 const botGroup = 'https://chat.whatsapp.com/D6bHVUjyGj06bb6iZeUsOI';
 //---
 const arrMenuDownloader = ['instagram - pengunduh foto/video ig', 'ig - cmd singkat Instagram', 'tiktok - pengunduh video/foto tiktok', 'tt - cmd singkat tiktok', 'play - cari dan play video/audio YouTube', 'ytmp3 - pengunduh YouTube audio', 'ytmp4 - pengunduh YouTube video'];
-const arrMenuAI = ['ai - Akses AI tercanggih dengan pengetahuan waktu nyata dan pembuat gambar', 'anidif - pembuat gambar anime/diffusion'];
+const arrMenuAI = ['ai - Akses AI tercanggih dengan pengetahuan waktu nyata', 'anidif - pembuat gambar anime/diffusion'];
 const arrMenuAnime = ['ongoing - list anime on-going', 'jadwal - list jadwal anime update'];
 const arrMenuTools = ['hd - Gambar menjadi hd', 'remini - gambar menjadi hdv2', 'upscale - gambar menjadi 4× lebih hd', 'kl - cmd singkat kalkulator', 'kalkulator - penghitung soal mtk dasar', 'upload - upload foto ke server telegra.ph'];
 const arrMenuFun = ['top - top pemain game', 'point - cek point kamu', 'nyerah - menyerah saat bermain', 'hint - bantuan saat bermain', 'tebakkata - game tebakkata', 'susunkata - game susunkata', 'slot - game taruhan slot', 'siapaaku - game tebak siapaaku', 'math - game mtk dasar', 'caklontong - game tebak2an nyeleneh', 'asahotak - game mengasah otak'];
